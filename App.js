5665c9da00c9c1a4bd966686fb2c21687c3e1387
PIYUSH GUPTA <piyush.gupta111019@gmail.com>
2019-03-06 23:33:16 +0530

    without RN success

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { SafeAreaView, Text } from "react-native";
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
+      <SafeAreaView style={{ flex:1, backgroundColor:'#fff', justifyContent:'center', alignItems:'center'}}>
+        <Swipeable>
+          <Text>hello mate</Text>
+        </Swipeable>
+      </SafeAreaView>
     );
   }
 }
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-});
-
-export default createAppContainer(AppNavigator);
+export default HomeScreen ;
--- jestsetup.js
+++ jestsetup.js
@@ -1,1 +1,16 @@
-// a
+import { NativeModules} from 'react-native';
+
+NativeModules.RNGestureHandlerModule = {
+  attachGestureHandler: jest.fn(),
+  createGestureHandler: jest.fn(),
+  dropGestureHandler: jest.fn(),
+  updateGestureHandler: jest.fn(),
+  forceTouchAvailable: jest.fn(),
+  State: {},
+  Directions: {}
+};
+
+
+NativeModules.PlatformConstants = {
+  forceTouchAvailable: false,
+};
